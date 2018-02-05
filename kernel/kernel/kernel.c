1d01d7edf1ca41b004f03d3a7282f66ee025732d
Toby Hughes <tobywhughes@gmail.com>
2018-02-05 12:56:17 -0800

    Added integer printing with itoa now in stdlib, also changed bash scripts to automatically clean if flag not provided

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <kernel/tty.h>
 #include <arch/i386/vga.h>
 
 void kernel_main(void) {
 	terminal_initialize(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK);
-	printf("Hello, kernel World!\nAnd hello newline");
+	printf("STARTING TESTING SEQUENCE\n\n");
+	printf("NUMBER TEST:\n\n");
+	printf("Decimal\n\n");
+	int negative = -1234;
+	int zero = 0;
+	printf("%d\n", negative);
+	printf("%d\n", zero);
+	for(int i = 10; i < 1000000; i = i * 10)
+	{
+		printf("%d\n", i); 
+	}
 }
